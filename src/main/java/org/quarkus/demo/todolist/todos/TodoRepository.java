3339eda5251fb0c78d217127cc29bbf7c888f682
Yeray Borges <yborgess@redhat.com>
2023-12-26 22:24:53 +0000

    Clean up unused UriInfo from TodoRepository

--- src/main/java/org/quarkus/demo/todolist/todos/TodoRepository.java
+++ src/main/java/org/quarkus/demo/todolist/todos/TodoRepository.java
@@ -2,21 +2,11 @@ package org.quarkus.demo.todolist.todos;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import jakarta.persistence.LockModeType;
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.UriInfo;
-
-import java.util.Optional;
 
 
 @ApplicationScoped
 public class TodoRepository implements PanacheRepository<Todo> {
-
-    @Context
-    UriInfo uriInfo;
-
     public Todo findTodoById(Long id) {
         Todo result = findById(id);
         if (result == null) {
